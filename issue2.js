bc0caedcf84357020790c6713b08a2cd8d185b13
ayumuSasagawa <a_sasagawa@uluru.jp>
2021-05-27 11:51:29 +0900

    課題2-b完了

--- issue2.js
+++ issue2.js
@@ -23,6 +23,7 @@ class Counter {
 
 }
 
+//Counterクラス
 let counter1 = new Counter;
 let counter2 = new Counter;
 
@@ -45,3 +46,33 @@ counter2.resetValue();
 console.log(counter2.getValue());
 counter2.up();
 console.log(counter2.getValue());
+
+
+class EvenCounter {
+ constructor() {
+   this.count = 0;
+   this.evenCounter = 0;
+ }
+
+ up() {
+   this.evenCounter = this.evenCounter + 1;
+   if (this.evenCounter % 2 === 0) {
+     this.count = this.count + 1;
+   }
+ }
+
+ getValue() {
+   return this.count;
+ }
+
+}
+
+console.log("---EvenCounter--");
+let counter = new EvenCounter;
+counter.up();
+counter.up();
+console.log(counter.getValue());
+counter.up();
+counter.up();
+counter.up();
+console.log(counter.getValue());
