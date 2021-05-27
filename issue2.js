4e7a35050925ce08abe5130f203d01f839f3f509
ayumuSasagawa <a_sasagawa@uluru.jp>
2021-05-27 11:45:41 +0900

    課題2-c完了

--- issue2.js
+++ issue2.js
@@ -23,13 +23,25 @@ class Counter {
 
 }
 
-let counter = new Counter;
-counter.up();
-console.log(counter.getValue());
-counter.up();
-console.log(counter.getValue());
-counter.down();
-console.log(counter.getValue());
-counter.resetValue();
-console.log(counter.getValue());
+let counter1 = new Counter;
+let counter2 = new Counter;
 
+console.log("---counter1---");
+counter1.up();
+console.log(counter1.getValue());
+counter1.up();
+console.log(counter1.getValue());
+counter1.down();
+console.log(counter1.getValue());
+counter1.resetValue();
+console.log(counter1.getValue());
+
+console.log("---counter2---");
+counter2.down();
+console.log(counter2.getValue());
+counter2.down();
+console.log(counter2.getValue());
+counter2.resetValue();
+console.log(counter2.getValue());
+counter2.up();
+console.log(counter2.getValue());
